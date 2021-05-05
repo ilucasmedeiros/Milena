1255c3c6d89bd903088b37f3e061a97168bb0650
Lucas Rezende <lucas-mr@hotmail.com.br>
2021-05-05 16:27:39 -0300

    Entrega Final

--- assets/contact.php
+++ assets/contact.php
@@ -18,34 +18,34 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "your@gmail.com";
+        $recipient = "milenapmartins1@hotmail.com";
 
         // Set the email subject.
         $subject = "$name";
 
         // Build the email content.
-        $email_content = "First Name: $name\n";
+        $email_content = "Nome: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
+        $email_content .= "Mensagem:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "De: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Obrigada! Sua mensagem foi enviada.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Ups! Algo deu errado e não foi possível enviar sua mensagem.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Ocorreu um problema com o seu envio, tente novamente.";
     }
 
 ?>
